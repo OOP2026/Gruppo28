6690f55318972d5807cca60908d8fdc056244883
ENZO-1935 <vince.didonato@studenti.unina.it>
2026-05-07 17:35:14 +0200

    Creato la classe Studente

---
+++ src/main/java/model/Studente.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Studente extends Utente {
+
+    private int matricola;
+
+    /**
+     *
+     * @param ArgomrntoTirocinio
+     */
+    public void richiediTirocinio(int ArgomrntoTirocinio) {
+        // TODO - implement Studente.richiediTirocinio
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param File
+     * @param SedutaLaurea
+     */
+    public void caricaTesi(int File, int SedutaLaurea) {
+        // TODO - implement Studente.caricaTesi
+        throw new UnsupportedOperationException();
+    }
+
+}
