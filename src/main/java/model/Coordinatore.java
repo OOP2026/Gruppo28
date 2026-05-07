d7bab7fc8979edfda8c3ba486bd6a8a677e6aed5
ENZO-1935 <vince.didonato@studenti.unina.it>
2026-05-07 17:35:57 +0200

    Creato la classe Coordinatore

---
+++ src/main/java/model/Coordinatore.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Coordinatore extends Docente {
+
+    /**
+     *
+     * @param SedutaLaurea
+     */
+    public void inserisciSeduta(int SedutaLaurea) {
+        // TODO - implement Coordinatore.inserisciSeduta
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param SedutaLaurea
+     */
+    public List<Docente> formaCommissione(int SedutaLaurea) {
+        // TODO - implement Coordinatore.formaCommissione
+        throw new UnsupportedOperationException();
+    }
+
+}
