c3e178cb4119b2af8aaf50b16189ed47bcc7c82b
ENZO-1935 <vince.didonato@studenti.unina.it>
2026-05-07 17:35:44 +0200

    Creato la classe Docente

---
+++ src/main/java/model/Docente.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Docente {
+
+    /**
+     *
+     * @param ArgomentoTirocinio
+     */
+    public void aggiungiArgomento(int ArgomentoTirocinio) {
+        // TODO - implement Docente.aggiungiArgomento
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param RichiestaTirocinio
+     */
+    public void valutaRichiesta(boolean RichiestaTirocinio) {
+        // TODO - implement Docente.valutaRichiesta
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param Tesi
+     */
+    public void valutaTesi(boolean Tesi) {
+        // TODO - implement Docente.valutaTesi
+        throw new UnsupportedOperationException();
+    }
+
+    public List<RichiestaTirocinio> getTirocinilnCorso() {
+        // TODO - implement Docente.getTirocinilnCorso
+        throw new UnsupportedOperationException();
+    }
+
+}
+
